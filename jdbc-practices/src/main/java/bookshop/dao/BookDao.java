f89bdae9d852c1c7547a86158085e5354605ed0f
청출어람_PC14 <_PC14@DESKTOP-OT013FC>
2022-05-31 11:57:08 +0900

    commit

--- jdbc-practices/src/main/java/bookshop/dao/BookDao.java
+++ jdbc-practices/src/main/java/bookshop/dao/BookDao.java
@@ -12,43 +12,40 @@ import bookshop.vo.BookVo;
 
 public class BookDao {
 
-	public void update(BookVo vo) {
-	
-		public boolean insert(BookVo vo) {
-			boolean result = false;
-			Connection connection = null;
-			PreparedStatement pstmt = null;
+	public boolean insert(BookVo vo) {
+		boolean result = false;
+		Connection connection = null;
+		PreparedStatement pstmt = null;
+		
+		try {
+			connection = getConnection();
 			
+			String sql = "insert into book values(null, ?, ?, ?)";
+			pstmt = connection.prepareStatement(sql);
+			pstmt.setString(1, vo.getTitle());
+			pstmt.setString(2, vo.getStateCode());
+			pstmt.setLong(3, vo.getAuthorNo());
+			
+			int count = pstmt.executeUpdate();
+			result = count == 1;
+		} catch (SQLException e) {
+			System.out.println("드라이버 로딩 실패:" + e);
+		} finally {
 			try {
-				connection = getConnection();
-				
-				String sql = "update book values(null, ?, ?, ?)";
-				pstmt = connection.prepareStatement(sql);
-				pstmt.setString(1, vo.getTitle());
-				pstmt.setString(2, vo.getStateCode());
-				pstmt.setLong(3, vo.getAuthorNo());
-				
-				int count = pstmt.executeUpdate();
-				result = count == 1;
-			} catch (SQLException e) {
-				System.out.println("드라이버 로딩 실패:" + e);
-			} finally {
-				try {
-					if(pstmt != null) {
-						pstmt.close();
-					}
-					if(connection != null) {
-						connection.close();
-					}
-				} catch (SQLException e) {
-					e.printStackTrace();
+				if(pstmt != null) {
+					pstmt.close();
 				}
+				if(connection != null) {
+					connection.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
 			}
-			
-			return result;		
 		}
+		
+		return result;		
 	}
-	
+
 	public List<BookVo> findAll() {
 		List<BookVo> result = new ArrayList<>();
 		Connection connection = null;
@@ -101,12 +98,102 @@ public class BookDao {
 		return result;		
 	}
 	
+	public void update(Long no, String stateCode) {
+		BookVo vo = new BookVo();
+		vo.setNo(no);
+		vo.setStateCode(stateCode);
+		
+		update(vo);
+	}
+	
+	public boolean update(BookVo vo) {
+		boolean result = false;
+		Connection connection = null;
+		PreparedStatement pstmt = null;
+		
+		try {
+			connection = getConnection();
+			
+			String sql = "update book set state_code=? where no=?";
+			pstmt = connection.prepareStatement(sql);
+			pstmt.setString(1, vo.getStateCode());
+			pstmt.setLong(2, vo.getNo());
+			
+			int count = pstmt.executeUpdate();
+			result = count == 1;
+		} catch (SQLException e) {
+			System.out.println("드라이버 로딩 실패:" + e);
+		} finally {
+			try {
+				if(pstmt != null) {
+					pstmt.close();
+				}
+				if(connection != null) {
+					connection.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		return result;		
+	}
+	
+	public BookVo findByNo(long no) {
+		BookVo result = null;
+		Connection connection = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		try {
+			connection = getConnection();
+
+			String sql =
+				"select a.no, a.title, a.author_no, b.name, a.state_code" +
+				"  from book a, author b" +
+				" where a.author_no = b.no" +
+				"   and a.no=?";
+			pstmt = connection.prepareStatement(sql);
+			pstmt.setLong(1, no);
+			
+			rs = pstmt.executeQuery();
+			
+			if(rs.next()) {
+				result = new BookVo();
+				
+				result.setNo(rs.getLong(1));
+				result.setTitle(rs.getString(2));
+				result.setAuthorNo(rs.getLong(3));
+				result.setAuthorName(rs.getString(4));
+				result.setStateCode(rs.getString(5));
+			}
+		} catch (SQLException e) {
+			System.out.println("드라이버 로딩 실패:" + e);
+		} finally {
+			try {
+				if(rs != null) {
+					rs.close();
+				}
+				if(pstmt != null) {
+					pstmt.close();
+				}
+				if(connection != null) {
+					connection.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		return result;
+	}
+	
 	private Connection getConnection() throws SQLException {
 		Connection connection = null;
 		
 		try {
 			Class.forName("org.mariadb.jdbc.Driver");
-			String url = "jdbc:mysql://192.168.10.34:3306/webdb?charset=utf8";
+			String url = "jdbc:mysql://192.168.10.55:3306/webdb?charset=utf8";
 			connection = DriverManager.getConnection(url, "webdb", "webdb");
 		} catch (ClassNotFoundException e) {
 			System.out.println("드라이버 로딩 실패:" + e);
@@ -114,7 +201,4 @@ public class BookDao {
 		
 		return connection;
 	}
-
-
-
 }
